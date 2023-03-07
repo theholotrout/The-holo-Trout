77e71d39d12239c69b032bd511d77c7e0ebb5c8b
Holotrout <theholotrout@gmail.com>
2023-03-07 09:21:13 -0500

    Swift
    Uploads and integrations

---
+++ SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Holotrout
+//
+//  Created by Holotrout on 1/1/23.
+//  Copyright © 2023 Holotrout. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
