19a7dc6b44f620fb47fc82244df88c13889f1f3c
Holotrout <theholotrout@gmail.com>
2023-03-29 03:29:37 -0400

    Add files via upload

---
+++ Holotrout_Package copy.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Holot",/Users/holotrout/Library/CloudStorage/OneDrive-Personal/Holotrout_Safari/Holotrout_Safari.xcodeproj
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Holot",
+            targets: ["Holot"]),
+    ], 
